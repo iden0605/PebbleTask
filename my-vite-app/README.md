55045fe877ca614cfd9ae2a8c34e85e01217e7a1
iden0605 <79390850+iden0605@users.noreply.github.com>
2025-03-30 04:20:51 +1100

    init commit

---
+++ my-vite-app/README.md
@@ -0,0 +1,7 @@
+downloads:
+npm install
+npm install canvas-confetti
+npm install react-dnd react-dnd-html5-backend
+
+run:
+npm run dev
---
+++ my-vite-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import TaskManager from './TaskManager';
+
+function App() {
+  return (
+    <div>
+      <TaskManager />
+    </div>
+  );
+}
+
+export default App
